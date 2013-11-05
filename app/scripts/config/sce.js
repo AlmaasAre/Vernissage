64c1b7a5928938b5b10cde67ab70e6a5e7ff2870
tcskrovseth <thoskr@gmail.com>
2013-11-05 14:59:01 +0100

    Added $SCE

---
+++ app/scripts/config/sce.js
@@ -0,0 +1,10 @@
+app
+    .config(['$sceDelegateProvider', function ($sceDelegateProvider) {
+    	$sceDelegateProvider.resourceUrlWhitelist([
+    		//Allow current domain
+			'self',
+			//Allow W3C test video
+			'http://www.w3schools.com/html/mov_bbb.mp4'
+		]);
+
+    }]);
