c9b8b6c58f5d3260e17f4d5809a320fafa10122a
Kristoffer Sundmyhr <kristoffer@sundmyhr.no>
2013-11-05 14:40:46 +0100

    Move social tile code to cv

--- app/scripts/controllers/cv.js
+++ app/scripts/controllers/cv.js
@@ -2,9 +2,95 @@
 
 app
   	.controller('CvCtrl', function ($scope) {
-    	$scope.awesomeThings = [
-      		'HTML5 Boilerplate',
-      		'AngularJS',
-      		'Karma'
+    	$scope.tiles = [
+      		{
+      			name: 'Navn Navnesen',
+      			dep: 'Front-end & Mobile',
+      			job: 'Front-end Developer',
+      			available: true,
+      			projects: [
+      				'Vernissage',
+      				'Statens Kartverk',
+      				'Haavind',
+      				'Internal'
+      			]
+      		},
+      		{
+      			name: 'Navn Navnesen',
+      			dep: 'Front-end & Mobile',
+      			job: 'Front-end Developer',
+      			projects: [
+      				'Vernissage',
+      				'Statens Kartverk',
+      				'Haavind',
+      				'Internal'
+      			]
+      		},
+      		{
+      			name: 'Navn Navnesen',
+      			dep: 'Front-end & Mobile',
+      			job: 'Front-end Developer',
+      			projects: [
+      				'Vernissage',
+      				'Statens Kartverk',
+      				'Haavind',
+      				'Internal'
+      			]
+      		},
+      		{
+      			name: 'Navn Navnesen',
+      			dep: 'Front-end & Mobile',
+      			job: 'Front-end Developer',
+      			projects: [
+      				'Vernissage',
+      				'Statens Kartverk',
+      				'Haavind',
+      				'Internal'
+      			]
+      		},
+      		{
+      			name: 'Navn Navnesen',
+      			dep: 'Front-end & Mobile',
+      			job: 'Front-end Developer',
+      			projects: [
+      				'Vernissage',
+      				'Statens Kartverk',
+      				'Haavind',
+      				'Internal'
+      			]
+      		},
+      		{
+      			name: 'Navn Navnesen',
+      			dep: 'Front-end & Mobile',
+      			job: 'Front-end Developer',
+      			projects: [
+      				'Vernissage',
+      				'Statens Kartverk',
+      				'Haavind',
+      				'Internal'
+      			]
+      		},
+      		{
+      			name: 'Navn Navnesen',
+      			dep: 'Front-end & Mobile',
+      			job: 'Front-end Developer',
+      			projects: [
+      				'Vernissage',
+      				'Statens Kartverk',
+      				'Haavind',
+      				'Internal'
+      			]
+      		},
+      		{
+      			name: 'Navn Navnesen',
+      			dep: 'Front-end & Mobile',
+      			job: 'Front-end Developer',
+      			projects: [
+      				'Vernissage',
+      				'Statens Kartverk',
+      				'Haavind',
+      				'Internal'
+      			]
+      		}
     	];
   	});
