f718eb9e9721ed7ae2375a667dbbce3cb1ae5cf2
Kristoffer Sundmyhr <kristoffer@sundmyhr.no>
2013-11-05 15:44:41 +0100

    Add better dummy data to cv

--- app/scripts/controllers/cv.js
+++ app/scripts/controllers/cv.js
@@ -46,9 +46,9 @@ app
       			]
       		},
       		{
-      			name: 'Helge Standal',
-      			dep: 'Front-end & Mobile',
-      			img: 'http://labs.makingwaves.com/customer/internal/employees/data/img/2x_a504b03dedf0e0c7407eb3d88404c7783a6dfeea.jpg',
+      			name: 'Adam Dziawgo',
+      			dep: 'ITO',
+      			img: 'http://labs.makingwaves.com/customer/internal/employees/data/img/2x_6737384905f2e70149dafe29df7388009474f10b.jpg',
       			job: 'Front-end Developer',
       			available: false,
       			custom: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda, magnam.',
@@ -60,9 +60,9 @@ app
       			]
       		},
       		{
-      			name: 'Are Almaas',
-      			dep: 'Front-end & Mobile',
-      			img: 'http://labs.makingwaves.com/customer/internal/employees/data/img/2x_fcd69278309385eb145351f8e4a4c3eef092e4ef.jpg',
+      			name: 'Andreas Fjellstad Dahle',
+      			dep: 'Application Management',
+      			img: 'http://labs.makingwaves.com/customer/internal/employees/data/img/2x_f93b09e1b15dd0df09831e689f15458a8706605e.jpg',
       			job: 'Front-end Developer',
       			available: false,
       			custom: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis, exercitationem!',
@@ -74,9 +74,9 @@ app
       			]
       		},
       		{
-      			name: 'Christian Skrøvseth',
-      			dep: 'Front-end & Mobile',
-      			img: 'http://labs.makingwaves.com/customer/internal/employees/data/img/2x_471fbc5fb81368db4ff69c7a1850d80c5157fcc0.jpg',
+      			name: 'Dominik Juszczyk',
+      			dep: 'Application Management',
+      			img: 'http://labs.makingwaves.com/customer/internal/employees/data/img/2x_a76a040a427077e0452dc6f3231c992ea45c462d.jpg',
       			job: 'Front-end Developer',
       			available: true,
       			custom: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est, explicabo.',
@@ -88,9 +88,9 @@ app
       			]
       		},
       		{
-      			name: 'Helge Standal',
-      			dep: 'Front-end & Mobile',
-      			img: 'http://labs.makingwaves.com/customer/internal/employees/data/img/2x_a504b03dedf0e0c7407eb3d88404c7783a6dfeea.jpg',
+      			name: 'Arne Bakkebø',
+      			dep: 'Application Management',
+      			img: 'http://labs.makingwaves.com/customer/internal/employees/data/img/2x_1a8fbec45f4efe81d3ef88bb76919856771bd91c.jpg',
       			job: 'Front-end Developer',
       			available: true,
       			custom: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, totam.',
@@ -102,9 +102,9 @@ app
       			]
       		},
       		{
-      			name: 'Are Almaas',
+      			name: 'Ewelina Czarny',
       			dep: 'Front-end & Mobile',
-      			img: 'http://labs.makingwaves.com/customer/internal/employees/data/img/2x_fcd69278309385eb145351f8e4a4c3eef092e4ef.jpg',
+      			img: 'http://labs.makingwaves.com/customer/internal/employees/data/img/2x_0629789b936766103b699d64687803e37a827a0c.jpg',
       			job: 'Front-end Developer',
       			available: true,
       			custom: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, omnis.',
