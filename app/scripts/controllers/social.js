25a9a43fff5c6672a7b366667a4a64314fc0cce0
tcskrovseth <thoskr@gmail.com>
2013-11-05 14:59:38 +0100

    Social initial work

--- app/scripts/controllers/social.js
+++ app/scripts/controllers/social.js
@@ -4,8 +4,24 @@ app
   	.controller('SocialCtrl', function ($scope) {
     	$scope.contents = [
       		{
-      			url: 'http://placekitten.com/640/800',
+      			url: 'http://placekitten.com/320/180',
       			video: false
+      		},
+      		{
+      			url: 'http://placekitten.com/320/180',
+      			video: false
+      		},
+      		{
+      			url: 'http://placekitten.com/320/180',
+      			video: false
+      		},
+      		{
+      			url: 'http://placekitten.com/320/180',
+      			video: false
+      		},
+      		{
+      			url: 'http://www.w3schools.com/html/mov_bbb.mp4',
+      			video: true
       		}
     	];
   	});
