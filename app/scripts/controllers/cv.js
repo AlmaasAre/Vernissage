3e2749a83516b1f6c8b4740fe6aa71cd6397b910
Kristoffer Sundmyhr <kristoffer@sundmyhr.no>
2013-11-06 16:04:31 +0100

    Add multiple news func, add animation to cv

--- app/scripts/controllers/cv.js
+++ app/scripts/controllers/cv.js
@@ -2,12 +2,11 @@
 
 app
   	.controller('CvCtrl', function ($scope, $timeout) {
-    	$scope.animationClass = "";
+    	$scope.animationClass = "flipInY";
 
     	$scope.people = [
       		{
       			name: 'Helge Standal',
-      			number: 1,
       			dep: 'Department',
       			img: 'http://labs.makingwaves.com/customer/internal/employees/data/img/2x_a504b03dedf0e0c7407eb3d88404c7783a6dfeea.jpg',
       			job: 'Title',
@@ -22,7 +21,6 @@ app
       		},
       		{
       			name: 'Are Almaas',
-      			number: 2,
       			dep: 'Department',
       			img: 'http://labs.makingwaves.com/customer/internal/employees/data/img/2x_fcd69278309385eb145351f8e4a4c3eef092e4ef.jpg',
       			job: 'Title',
@@ -37,7 +35,6 @@ app
       		},
       		{
       			name: 'Asbjørn Vølstad',
-      			number: 3,
       			dep: 'Department',
       			img: 'http://labs.makingwaves.com/customer/internal/employees/data/img/2x_3fb7e5d8697057f204b25b950986a603ec3d3099.jpg',
       			job: 'Title',
@@ -52,7 +49,6 @@ app
       		},
       		{
       			name: 'Adam Dziawgo',
-      			number: 4,
       			dep: 'Department',
       			img: 'http://labs.makingwaves.com/customer/internal/employees/data/img/2x_6737384905f2e70149dafe29df7388009474f10b.jpg',
       			job: 'Title',
@@ -67,7 +63,6 @@ app
       		},
       		{
       			name: 'Andreas Fjellstad Dahle',
-      			number: 5,
       			dep: 'Department',
       			img: 'http://labs.makingwaves.com/customer/internal/employees/data/img/2x_f93b09e1b15dd0df09831e689f15458a8706605e.jpg',
       			job: 'Title',
@@ -82,7 +77,6 @@ app
       		},
       		{
       			name: 'Dominik Juszczyk',
-      			number: 6,
       			dep: 'Department',
       			img: 'http://labs.makingwaves.com/customer/internal/employees/data/img/2x_a76a040a427077e0452dc6f3231c992ea45c462d.jpg',
       			job: 'Title',
@@ -97,57 +91,11 @@ app
       		},
       		{
       			name: 'Arne Bakkebø',
-      			number: 7,
       			dep: 'Department',
       			img: 'http://labs.makingwaves.com/customer/internal/employees/data/img/2x_1a8fbec45f4efe81d3ef88bb76919856771bd91c.jpg',
       			job: 'Title',
       			available: true,
       			custom: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, totam.',
-      			projects: [
-      				'Vernissage',
-      				'Statens Kartverk',
-      				'Haavind',
-      				'Internal'
-      			]
-      		},
-      		{
-      			name: 'Ewelina Czarny',
-      			number: 8,
-      			dep: 'Department',
-      			img: 'http://labs.makingwaves.com/customer/internal/employees/data/img/2x_0629789b936766103b699d64687803e37a827a0c.jpg',
-      			job: 'Title',
-      			available: true,
-      			custom: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, omnis.',
-      			projects: [
-      				'Vernissage',
-      				'Statens Kartverk',
-      				'Haavind',
-      				'Internal'
-      			]
-      		},
-      		{
-      			name: 'Arne Bakkebø',
-      			number: 9,
-      			dep: 'Department',
-      			img: 'http://labs.makingwaves.com/customer/internal/employees/data/img/2x_1a8fbec45f4efe81d3ef88bb76919856771bd91c.jpg',
-      			job: 'Title',
-      			available: true,
-      			custom: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, totam.',
-      			projects: [
-      				'Vernissage',
-      				'Statens Kartverk',
-      				'Haavind',
-      				'Internal'
-      			]
-      		},
-      		{
-      			name: 'Ewelina Czarny',
-      			number: 10,
-      			dep: 'Department',
-      			img: 'http://labs.makingwaves.com/customer/internal/employees/data/img/2x_0629789b936766103b699d64687803e37a827a0c.jpg',
-      			job: 'Title',
-      			available: true,
-      			custom: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, omnis.',
       			projects: [
       				'Vernissage',
       				'Statens Kartverk',
@@ -165,7 +113,7 @@ app
 
     	function shuffle(o) { //v1.0
 		    for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
-	    	console.log("** Shuffled. **");
+	    	// console.log("** Shuffled. **");
 		    return o;
 		};
 
@@ -181,7 +129,7 @@ app
 					$scope.tmpNewPeople.push($scope.people[i]);
 				}
 
-				console.log("** Kjørt gjennom alle people **");
+				// console.log("** Kjørt gjennom alle people **");
 			}
 
 			$scope.newPeople = shuffle($scope.tmpNewPeople);
@@ -190,35 +138,29 @@ app
 				$scope.selectedTiles.push($scope.newPeople[i]);
 			}
 
-			console.log("Populate count: " + $scope.populateCount);
-			console.log("Selected tiles: " + $scope.selectedTiles.length);
-			console.log("Tmp new people: " + $scope.tmpNewPeople.length);
-			console.log("New people: " + $scope.newPeople.length);
-			console.log("People: " + $scope.people.length);
+			// console.log("Populate count: " + $scope.populateCount);
+			// console.log("Selected tiles: " + $scope.selectedTiles.length);
+			// console.log("Tmp new people: " + $scope.tmpNewPeople.length);
+			// console.log("New people: " + $scope.newPeople.length);
+			// console.log("People: " + $scope.people.length);
 		}
 
 		$scope.populateTiles();
 
-    	$scope.$watch('animationClass', function() {
-    		var t = $timeout(function() {
+		var si = setInterval(function() {
+    		$scope.animationClass = "flipInY";
+    		$scope.$apply();
+    	}, 1000);
 
-    			var to = $timeout(function() {
-	    			$scope.populateTiles();
-	    		}, (7100));
-	    		// $timeout.cancel(to);
+		var sin = setInterval(function() {
+			var t = $timeout(function() {
+				$scope.populateTiles();
+			}, 1200);
+		}, 8000);
 
-	    		var tou = $timeout(function() {
-		    		$scope.animationClass = "flipInY";
-		    		
-		    		var tout = $timeout(function() {
-			    		$scope.animationClass = "flipOutY";
-			    	}, 5000);
-		    		// $timeout.cancel(tout);
+		var sint = setInterval(function() {
+    		$scope.animationClass = "fadeOut";
+    		$scope.$apply();
+    	}, 8000);
 
-		    	}, 1000);
-		    	// $timeout.cancel(tou);
-
-    		}, 4000);
-    		// $timeout.cancel(t);
-    	});
   	});
--- app/scripts/controllers/news.js
+++ app/scripts/controllers/news.js
@@ -2,9 +2,41 @@
 
 app
   	.controller('NewsCtrl', function ($scope) {
-    	$scope.awesomeThings = [
-      		'HTML5 Boilerplate',
-     		'AngularJS',
-      		'Karma'
+    	$scope.news = [
+	    	{
+	    		date: "Today",
+	    		title: "Remember the Foosball competition!"
+	    	},
+	    	{
+	    		date: "Tomorrow",
+	    		title: "Weekend!"
+	    	},
+	    	{
+	    		date: "Next sunday",
+	    		title: "Somehting that will happen then"
+	    	}
     	];
+
+    	$scope.counter = -1;
+    	$scope.currentNews = $scope.news[$scope.counter];
+    	$scope.newsItemLength = 12000;
+
+    	$scope.initNews = function() {
+    		if ($scope.counter === ($scope.news.length)-1) {
+    			$scope.counter = 0;
+    			$scope.$apply();
+    		} else {
+    			$scope.counter++;
+    			$scope.$apply();
+    		}
+
+    		$scope.currentNews = $scope.news[$scope.counter];
+    		$scope.$apply();
+
+    		$('.news-inner').css('margin-top', (($('.news-inner').outerHeight()/2)*-1)-35);
+    	}
+
+    	var si = setInterval(function() {
+    		$scope.initNews();
+    	}, $scope.newsItemLength);
   	});
